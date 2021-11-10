3f15762335987ea0fbd41551501d66c58fd064b0
Jacob McCarthy <thejacobmccarthy@gmail.com>
2021-11-10 15:56:37 -0500

    fixed the app, now just cleaning up the file structure

---
+++ server.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+const uuid = require('./helpers/uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('./helpers/fsUtils');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+app.use(express.static('public'));
+
+const notesDb = require('./db/db.json');
+
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+
+
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/api/notes', (req, res) =>{
+
+  const {title, text} = req.body;
+
+  if(title && text) {
+
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
+
+
+      writeToFile('./db/db.json', result);
+
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
